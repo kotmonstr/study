23065dfff304028e0c81b88d977e6732f7c24f16
Sergey N. Sytnic <sytnic@sadesign.com.ua>
2014-04-16 18:08:09 +0400

    video+

--- protected/views/video/index.php
+++ protected/views/video/index.php
@@ -4,3 +4,10 @@
 
 <h1>Video</h1>
 
+<div>
+<div id="main_wrapper" style="position: absolute; width: 300px; height: 300px; overflow: hidden;">
+<object type="application/x-shockwave-flash" id="flvplayer_videoHost" data="/flash2v/container.swf?i=800" width="50%" height="50%"><param name="menu" value="false"><param name="scale" value="showall"><param name="allowFullscreen" value="true"><param name="allowScriptAccess" value="always"><param name="bgcolor" value="#000000"><param name="wmode" value="opaque"><param name="flashvars" value="showZoomBtn=false&amp;showPopUpBtn=true&amp;showFullScreenBtn=true&amp;isPlay=true&amp;LCSession=&amp;domain_url=http://player.rutv.ru&amp;player_mode=1&amp;sid=r24&amp;testpic=http://cdn1.vesti.ru/vh/pictures/weight.gif&amp;ssl=false&amp;player=http://player.rutv.ru/iframe/live-play/id/21/&amp;skin=http://player.rutv.ru/iframe/live-config/sid/r24/&amp;iframe_src=http://player.rutv.ru/iframe/live/id/__id__/showZoomBtn/false/isPlay/false/&amp;showEmbed=false"></object>
+
+<object type="application/x-shockwave-flash" id="flvplayer_videoHost" data="/flash2v/container.swf?i=800" width="50%" height="50%"><param name="menu" value="false"><param name="scale" value="showall"><param name="allowFullscreen" value="true"><param name="allowScriptAccess" value="always"><param name="bgcolor" value="#0005500"><param name="wmode" value="opaque"><param name="flashvars" value="showZoomBtn=false&amp;showPopUpBtn=true&amp;showFullScreenBtn=true&amp;isPlay=true&amp;LCSession=&amp;domain_url=http://player.rutv.ru&amp;player_mode=1&amp;sid=rtrplaneta&amp;testpic=http://cdn1.vesti.ru/vh/pictures/weight.gif&amp;ssl=false&amp;player=http://player.rutv.ru/iframe/live-play/id/4941/&amp;skin=http://player.rutv.ru/iframe/live-config/sid/rtrplaneta/&amp;iframe_src=http://player.rutv.ru/iframe/live/id/__id__/showZoomBtn/false/isPlay/false/&amp;showEmbed=false"></object>
+</div>
+</div>
