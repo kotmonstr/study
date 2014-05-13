59f4716012b75c86574e4d4a459ed676def58d3b
Sergey N. Sytnic <sytnic@sadesign.com.ua>
2014-05-13 13:01:57 +0400

    comment

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -4,10 +4,7 @@ require_once('VarDumper.php');
 
 class PostController extends Controller {
 
-    /**
-     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-     * using two-column layout. See 'protected/views/layouts/column2.php'.
-     */
+
     public $layout = '//layouts/column2';
 
     /**
@@ -20,11 +17,7 @@ class PostController extends Controller {
         );
     }
 
-    /**
-     * Specifies the access control rules.
-     * This method is used by the 'accessControl' filter.
-     * @return array access control rules
-     */
+ 
     public function accessRules() {
         return array(
             array('allow', // allow all users to perform 'index' and 'view' actions
@@ -45,10 +38,7 @@ class PostController extends Controller {
         );
     }
 
-    /**
-     * Displays a particular model.
-     * @param integer $id the ID of the model to be displayed
-     */
+
     public function actionView($id) {
 
         //вывести коментаррии по id от post(criteria)  
@@ -86,10 +76,7 @@ class PostController extends Controller {
         return $this->_model;
     }
 
-    /**
-     * Creates a new model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     */
+
     public function actionCreate() {
         $model = new Post;
 
@@ -107,16 +94,10 @@ class PostController extends Controller {
         ));
     }
 
-    /**
-     * Updates a particular model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id the ID of the model to be updated
-     */
+
     public function actionUpdate($id) {
         $model = $this->loadModel($id);
 
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
 
         if (isset($_POST['Post'])) {
             $model->attributes = $_POST['Post'];
@@ -129,11 +110,6 @@ class PostController extends Controller {
         ));
     }
 
-    /**
-     * Deletes a particular model.
-     * If deletion is successful, the browser will be redirected to the 'admin' page.
-     * @param integer $id the ID of the model to be deleted
-     */
     public function actionDelete($id) {
         $this->loadModel($id)->delete();
 
@@ -171,18 +147,7 @@ class PostController extends Controller {
         ));
     }
 
-    /**
-     * Returns the data model based on the primary key given in the GET variable.
-     * If the data model is not found, an HTTP exception will be raised.
-     * @param integer $id the ID of the model to be loaded
-     * @return Post the loaded model
-     * @throws CHttpException
-     */
 
-    /**
-     * Performs the AJAX validation.
-     * @param Post $model the model to be validated
-     */
     protected function performAjaxValidation($model) {
         if (isset($_POST['ajax']) && $_POST['ajax'] === 'post-form') {
             echo CActiveForm::validate($model);
