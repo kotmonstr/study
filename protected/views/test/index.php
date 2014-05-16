e5140af87250f20c413727ce0d710c1e96527875
Sergey N. Sytnic <sytnic@sadesign.com.ua>
2014-05-16 16:30:04 +0400

    All work blog save+ flash +курица

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -9,7 +9,7 @@ $this->pageTitle = Yii::app()->name;
                     // инициализация сеанса
                     $ch = curl_init();
                     // установка URL и других необходимых параметров
-                    curl_setopt($ch, CURLOPT_URL, "www.sila-ra.com/rss.php");
+                    curl_setopt($ch, CURLOPT_URL, "http://www.sila-ra.orgfree.com/rss.php");
                     curl_setopt($ch, CURLOPT_HEADER, 0);
                     // загрузка страницы и выдача её браузеру
                     curl_exec($ch);
@@ -23,7 +23,7 @@ $this->pageTitle = Yii::app()->name;
                 
 //c помощью simpl exml - обьекта
 
-                    $xml = simplexml_load_file("http://www.sila-ra.com/rss-new.php");
+                    $xml = simplexml_load_file("http://www.sila-ra.orgfree.com/rss-new.php");
                     //VarDumper::dump($xml);
                     //print_r($xml);
 
--- protected/views/test/rss.php
+++ protected/views/test/rss.php
@@ -1,3 +1,5 @@
+
+
 <?php
 Yii::import('application.vendors.*');
 require_once('VarDumper.php');
@@ -5,16 +7,16 @@ $this->pageTitle=Yii::app()->name;
 ?> 
 <center>
     <?php
-    $headers = array('Content-type: text/html; charset=utf-8');
+    //$headers = array('Content-type: text/html; charset=utf-8');
     
     
 // инициализация сеанса
 $ch = curl_init();
 
 // установка URL и других необходимых параметров
-curl_setopt($ch, CURLOPT_URL, "http://www.vesti.ru/vesti.rss");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//curl_setopt($ch, CURLOPT_URL, "http://cityscan.ru/news.xml");
+//curl_setopt($ch, CURLOPT_HEADER, 0);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 // загрузка страницы и выдача её браузеру
 
 //curl_exec($ch);
@@ -26,20 +28,32 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 // завершение сеанса и освобождение ресурсов
 
-curl_close($ch);
+//curl_close($ch);
 
 
     ?>
 </center>
+
 <?php 
-  $xml = simplexml_load_file("http://www.vesti.ru/vesti.rss");
-                    //VarDumper::dump($xml);
+  $xml = simplexml_load_file("http://cityscan.ru/news.xml");
+                    VarDumper::dump($xml);
                     //print_r($xml);
  
             foreach ($xml->channel->item as $e) {
 
+                        echo $e->title;
+                        echo '<a href="'.$e->link.'">'.$e->link.'</a>';
                         echo $e->description;
-                        echo $e->link;
-                        echo $e->tirle;
+                        echo '<div>'.file_get_contents($e->link).'</div>'; 
                        
                     } ?>
+<!--
+// [guid] => '1570'
+//                [title] => 'СПб: Зимний международный Санкт-Петербургский театральный фестиваль (14-29 ноября)'
+//                [link] => 'http://cityscan.ru/news.php?view=1570'
+//                [description] => SimpleXMLElement#5
+//                (
+//                )
+//                [author] => 'albert@cityscan.ru'
+//                [category] => 'Прочие события'
+//                [enclosure] 
