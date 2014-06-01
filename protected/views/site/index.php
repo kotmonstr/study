d13bb500a27253988b9b3ad42023a30b35a647c8
kot <kot@192.168.0.103>
2014-06-02 02:27:56 +0400

    out-code

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,27 +1,6 @@
-<?php
-Yii::import('application.vendors.*');
-require_once('VarDumper.php');
+<center>
+<h3>Sila-ra.orgfree.com - ведическая коллекция знаний</h3>
+<h5>Коллекция самых актуальных ведических постов, видероликов, картиинок </h5>
+        <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pic3.png', 'Каталог ведических знаний', $htmlOptions=array('width'=>'500px','height'=>'300px'));?>
+</center>
 
-$this->pageTitle=Yii::app()->name;
-?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-
-<?php
-if(Yii::app()->user->name && Yii::app()->user->name !='Guest'){
-echo "Имя:".Yii::app()->user->name.'<br>';
-
-//VarDumper::dump( Yii::app()->user->name);die;
-
-$objUser = new User;
-echo "Your Id:".$objUser->getIdbyName(Yii::app()->user->name);
-}
