f8f0f69e1907069fcf9cf2a80a031ba53cfe48c3
kot <kot@192.168.0.103>
2014-06-02 03:21:27 +0400

    js

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,6 +1,18 @@
 <center>
 <h3>Sila-ra.orgfree.com - ведическая коллекция знаний</h3>
 <h5>Коллекция самых актуальных ведических постов, видероликов, картиинок </h5>
-        <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pic3.png', 'Каталог ведических знаний', $htmlOptions=array('width'=>'500px','height'=>'300px'));?>
+        <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pic3.png', 'Каталог ведических знаний', $htmlOptions=array('width'=>'500px','height'=>'300px','onmouseover'=>'Image.go()','style'=>'cursor:pointer','class'=>'image'))?>
 </center>
+<script>
+    var Image={
+        
+        go: function(){
+            console.log('Image.go');
+            $('.image').fadeOut('slow');
+        }
+    };
+    
+    
+    
+    </script>
 
