1af133b799303cc58cec0a14c8aa87e851edcf6b
kot <kot@192.168.0.102>
2014-06-02 19:09:15 +0400

    dump

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,18 +1,38 @@
 <center>
-<h3>Sila-ra.orgfree.com - ведическая коллекция знаний</h3>
-<h5>Коллекция самых актуальных ведических постов, видероликов, картиинок </h5>
-        <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pic3.png', 'Каталог ведических знаний', $htmlOptions=array('width'=>'500px','height'=>'300px','onmouseover'=>'Image.go()','style'=>'cursor:pointer','class'=>'image'))?>
+    <h3>Sila-ra.orgfree.com - ведическая коллекция знаний</h3>
+    <h5>Коллекция самых актуальных ведических постов, видероликов, картиинок </h5>
+    <?php echo CHtml::image(Yii::app()->baseUrl . '/images/pic3.png', 'Каталог ведических знаний', $htmlOptions = array('width' => '500px', 'height' => '300px', 'onmouseover' => 'Image.go()', 'style' => 'cursor:pointer', 'class' => 'image')) ?>
+    <br>
+    <?php echo CHtml::link('Видео ролики', array('youtubecode/lastslider'), $htmlOptions = array('class' => 'link')) ?>
+    <?php echo CHtml::link('Фотографии', array('video/slider'), $htmlOptions = array('class' => 'link')) ?>
+    <?php echo CHtml::link('Статьи', array('post/index'), $htmlOptions = array('class' => 'link')) ?>
+
+
 </center>
 <script>
-    var Image={
-        
-        go: function(){
+    var Image = {
+        go: function() {
             console.log('Image.go');
             $('.image').fadeOut('slow');
+            $('.link').show('slow');
         }
     };
-    
-    
-    
-    </script>
+
+
+
+</script>
+
+<style>
+    .link{
+        display:none;
+        margin-right:25px;
+       
+    }
+    .link:hover{
+       color:red;
+       font-size:16px;
+       font-weight:bold;
+    }
+
+</style>
 
